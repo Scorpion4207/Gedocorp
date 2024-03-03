34da163d26dce9a9691fd0afb30934c432355474
Scorpion4207 <telitsin.andrei2015@yandex.ru>
2024-03-03 19:23:30 +0700

    added animation hero all

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@
 
 const presentationScreen = document.getElementById('presentationScreen');
 const website = document.getElementById('website');
+const animationHero = document.getElementById('animation__hero-text');
+const animationHeroTeam = document.getElementById('animation__hero-team');
 
 document.addEventListener('scroll', () => {
     presentationScreen.style.zIndex = '-10';
@@ -9,6 +11,8 @@ document.addEventListener('scroll', () => {
     setTimeout(function() {
         presentationScreen.style.display = 'none'
     }, 200);
+    animationHero.style.display = 'block';  
+    animationHeroTeam.style.display = 'block';  
 });
 
 })()
