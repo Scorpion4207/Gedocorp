27fc49d8bd9585591ce4772c6fbbeae2860b373a
Scorpion4207 <telitsin.andrei2015@yandex.ru>
2024-03-05 12:41:18 +0700

    fix v1.12 present title

--- js/main.js
+++ js/main.js
@@ -17,6 +17,10 @@ document.addEventListener('scroll', () => {
         presentationScreen.style.inset= 'none'; 
         presentationScreen.style.height= '50%'; 
         presentationScreen.style.width= '50%'; 
+        if (document.documentElement.clientWidth > 450) return
+        presentationScreen.style.inset= 'none'; 
+        presentationScreen.style.height= '25%'; 
+        presentationScreen.style.width= '0%'; 
     } 
 
     if (presentationScreen.style.zIndex = '-10') return
